033aefb46b5faf5f91c54c4e40f7bcc6ba6a1df4
Kaushik-Iyer-skima <kaushik.iyer@skima.ai>
2024-05-16 21:50:03 +0530

    changing site names

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -26,7 +26,7 @@ function LoginPage() {
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#f5f5f5' }}>
-            <h1 style={{ fontSize: '2rem', color: '#333', marginBottom: '2rem' }}>Login Page</h1>
+            <h1 style={{ fontSize: '2rem', color: '#333', marginBottom: '2rem' }}>Turf-Stats-Logger</h1>
             <button style={{ padding: '1rem 2rem', fontSize: '1rem', color: '#fff', backgroundColor: '#4285f4', border: 'none', borderRadius: '4px', cursor: 'pointer' }} onClick={handleLogin}>Login with Google</button>
         </div>
     );
